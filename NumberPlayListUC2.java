c5b493d141814af0956bacfef8cad5bb943def08
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2021-08-26 12:32:30 +0530

    storing and printing of double list using map

---
+++ NumberPlayListUC2.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class NumberPlayListUC2 {
+	
+	public static void main(String[] args) {
+
+		List<Integer> myNumberList = new ArrayList<Integer>();
+		for(int i = 0; i < 5 ; i++) myNumberList.add(i);
+		
+		// Processing Stream 
+		myNumberList.stream().forEach(n ->{
+			System.out.println(" Stream For Each Value ::" +n);
+		});
+			
+			
+		//Process Stream, Apply Operation and Store Result
+		
+		Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
+		List<Double> doubleList = myNumberList.stream().map(toDoubleFunction).collect(Collectors.toList());
+		System.out.println("Printing Double List : " + doubleList);
+	}
+
+}
