a52f339941e9a3d7232089140d71a27f6c5cca82
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2021-08-26 11:18:19 +0530

    Delete JavaStreamDemo directory

--- JavaStreamDemo/src/main/java/com/NumberPlayList.java
+++
@@ -1,7 +0,0 @@
-package main.java.com;
-
-public class NumberPlayList {
-  public static void main(String[] args) {
-	   System.out.println("Welcome to the Number Play List Program Using Java Streams");
-	    }
-}
