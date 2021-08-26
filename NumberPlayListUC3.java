ac1a077d9b49eed0d36fd227bf37e8ad02bc70b5
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2021-08-26 12:35:46 +0530

    printing even double list using filter

---
+++ NumberPlayListUC3.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class NumberPlayListUC3{
+	
+	public static void main(String[] args) {
+
+		List<Integer> myNumberList = new ArrayList<Integer>();
+		for(int i = 0; i < 5 ; i++) myNumberList.add(i);
+		
+		// Processing Stream 
+		myNumberList.stream().forEach(n ->{
+			System.out.println(" Stream For Each Value ::" +n);
+		});
+			
+			
+		//Process Stream, Apply Operation and Store Result
+		
+		Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
+		Predicate<Integer> isEvenFunction = n -> n>0 && n%2 ==0;
+		
+		List<Double> doubleList = myNumberList.stream().filter(isEvenFunction).map(toDoubleFunction).collect(Collectors.toList());
+		System.out.println("Printing Double List : " + doubleList);
+	}
+
+}
